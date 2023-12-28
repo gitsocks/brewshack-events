89713d67495b3808316a76cbc2291d2b050ae091
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2023-12-28 21:44:14 +0200

    Enable routing to create new application

--- src/app/(app)/apps/create/page.tsx
+++ src/app/(app)/apps/create/page.tsx
@@ -1,11 +1,13 @@
 import { CreateApplicationForm } from "@/components/forms/CreateApplicationForm/CreateApplicationForm";
 
 import styles from './page.module.css';
+import Link from "next/link";
 
 const Page = () => {
     return (
         <main>
             <div className={styles.innerContainer}>
+                <p>⬅️ <Link href={{ pathname: '/' }}>Back</Link></p>
                 <h2>Create a new application</h2>
                 <p>The first step to start tracking your application&apos;s events.</p>
                 <CreateApplicationForm />
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -4,6 +4,8 @@ import { getUserFromServer } from "@/services/server/get-user-from-server";
 import { fetchAuthDetails } from "@/utils/fetch-auth-details";
 import Link from "next/link";
 
+import styles from './page.module.css';
+
 export default async function Home() {
   const authState = await fetchAuthDetails();
 
@@ -17,7 +19,10 @@ export default async function Home() {
 
   return (
     <AuthGuard user={authState}>
-      <h1>Applications</h1>
+      <div className={styles.header}>
+        <h1>Applications</h1>
+        <Link href={{ pathname: '/apps/create' }}><button>Create Application</button></Link>
+      </div>
       <p>Please select an application to view its events.</p>
       <ul>
         {currentUserIsValid() && currentUser?.applications?.map(application => (
